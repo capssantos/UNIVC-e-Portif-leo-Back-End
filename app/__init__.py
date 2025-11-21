a4a95e98567f2caef1ce2bf7f980b11719ca7fa9
Carlos Pereira <csantos.pereira@icloud.com>
2025-11-21 18:22:41 -0300

    Fix: Cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,25 +6,17 @@ from flasgger import Swagger
 def create_app():
     app = Flask(__name__)
 
-    # Libera explicitamente o front
-    CORS(
-        app,
-        resources={r"/*": {
-            "origins": [
-                "http://localhost:3000",
-                "http://127.0.0.1:3000",
-                "https://apiunivc.carlosp.dev",  # se quiser que o Swagger continue funcionando bonitinho
-            ]
-        }}
-    )
+    CORS(app, origins=["*"])
 
+    # Configuração básica do Swagger
     app.config["SWAGGER"] = {
         "title": "API Portifoleo UNIVC",
-        "uiversion": 3,
+        "uiversion": 3,  # Swagger UI v3
     }
 
-    Swagger(app)
+    Swagger(app)  # Inicializa o Swagger
 
+    # Importar e registrar blueprints
     from .routes.main_routes import main_bp
     from .routes.user_routes import user_bp
     from .routes.cursos_routes import cursos_bp
